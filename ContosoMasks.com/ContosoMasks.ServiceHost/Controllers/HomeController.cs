ae3a86a24b8826e11dda69c9e19de61c4a1ab970
Andy Wahrenberger <andywahr@microsoft.com>
2020-04-22 00:48:24 -0500

    need to have index

--- ContosoMasks.com/ContosoMasks.ServiceHost/Controllers/HomeController.cs
+++ ContosoMasks.com/ContosoMasks.ServiceHost/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace ContosoMasks.ServiceHost.Controllers
             _logger = logger;
         }
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         //public IActionResult Privacy()
         //{
